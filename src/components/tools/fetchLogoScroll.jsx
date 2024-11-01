b6fe3f1603a0cd40497cec9e0321fb9983d6afb5
salahelfatimi <salahfatimi16@gmail.com>
2024-11-01 21:35:14 +0100

    add

--- src/components/tools/fetchLogoScroll.jsx
+++ src/components/tools/fetchLogoScroll.jsx
@@ -1,4 +1,3 @@
-// app/components/FetchLogoScroll.js
 import Image from "next/image";
 
 export default function FetchLogoScroll() {
@@ -16,9 +15,9 @@ export default function FetchLogoScroll() {
   ];
 
   return (
-    <div class="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
-    <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
-      {images.map((src, index) => ( // Duplicate the images for smooth loop
+    <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
+    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+      {images.map((src, index) => ( 
         <li key={index} >
           <Image
               title={src.title}
@@ -31,7 +30,7 @@ export default function FetchLogoScroll() {
         </li>
       ))}
     </ul>
-    <ul class="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
+    <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll" aria-hidden="true">
       {images.map((src, index) => ( 
         <li key={index} >
           <Image

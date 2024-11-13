0be685727b664422a84f70966427916bf6c314f1
salahelfatimi <salahfatimi16@gmail.com>
2024-11-13 22:30:36 +0100

    add

--- src/app/appel/page.jsx
+++ src/app/appel/page.jsx
@@ -3,6 +3,40 @@ import FetchLogo from '@/components/tools/fetchlogo'
 import CalendlyEmbed from "@/components/tools/CalendlyWidget";
 import { Poppins } from "next/font/google";
 const poppins = Poppins ({ subsets: ["latin-ext"], weight:['900'] });
+
+export const metadata = {
+    title: "TRAFIC GENIUS | Appel",
+    description: "Découvrez comment créer une boutique en ligne rentable avec TRAFIC GENIUS . Suivez des stratégies étape par étape pour lancer et développer votre entreprise e-commerce. Commencez dès aujourd'hui et libérez le potentiel de l'entrepreneuriat en ligne !",
+    metadataBase: new URL(`${process.env.NEXT_PUBLIC_BASE_URL}`,),
+    keywords: ["création de boutique en ligne, entrepreneuriat en ligne, booster votre e-commerce, stratégies de vente en ligne, réussir son e-commerce, conseils pour boutique en ligne, lancer une boutique e-commerce"],
+    alternates: {
+      canonical: "/appel",
+  
+      
+    },
+    openGraph: {
+      title: "TRAFIC GENIUS | Votre clé pour réussir votre boutique en ligne",
+      description: "BOOSTEZ VOS VENTES DÈS DEMAIN EN TRANSFORMANT VOTRE SYSTÈME DE VENTE EN AIMANT À CLIENTS !",
+      url:`${process.env.NEXT_PUBLIC_BASE_URL}`,
+      robots: {
+        index: true,
+        follow: true,
+      },
+      siteName: "TRAFIC GENIUS | Votre clé pour réussir votre boutique en ligne",
+      images: [
+        {
+          url: `/opengraph-image.jpg`,
+          secureUrl: `/opengraph-image.jpg`,
+          width: 1200,
+          height: 675,
+          alt: `TRAFIC GENIUS`,
+        }
+      ],
+      type: "website",
+     
+    },
+  };
+
 export default  function  Appel(){
  
     return(

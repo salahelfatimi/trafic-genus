994c71797e522c009f27c3bf0077c925e4a790e0
salahelfatimi <salahfatimi16@gmail.com>
2024-11-20 17:46:28 +0100

    add sitmap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -17,6 +17,21 @@ export default async function sitemap() {
         url: `${process.env.NEXT_PUBLIC_BASE_URL}boutique`,
         lastModified: new Date(),
       }
+      ,
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}mentions-legales`,
+        lastModified: new Date(),
+      }
+      ,
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}conditions-generales-de-vente`,
+        lastModified: new Date(),
+      }
+      ,
+      {
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}clause-de-non-responsabilite`,
+        lastModified: new Date(),
+      }
       
     ];
   }

836ff0f5f3f1e7191e5dc53c40892ea910926fd3
salahelfatimi <salahfatimi16@gmail.com>
2024-11-23 19:37:38 +0100

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,8 @@
 
-import FacebookPixel from "@/components/tools/facebookPixel";
 import "./globals.css";
 import Footer from "@/components/footer/page";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from "next/script";
 
 
 
@@ -44,9 +44,26 @@ export default function RootLayout({ children }) {
   
   return (
     <html lang="en">
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+        __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID});
+        fbq('track', 'PageView');
+        `,
+        }}
+      />
       <GoogleAnalytics gaId={process.env.ANALYTICS_ID} />
       <body className={` scroll-smooth   scrollbar scrollbar-thumb-[#F2FD01] scrollbar-track-black h-32 overflow-y-scroll`}>
-        <FacebookPixel />
         {children}
         <Footer/>
       </body>

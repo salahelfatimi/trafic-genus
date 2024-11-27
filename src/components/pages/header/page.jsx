1164760aeb41dc85375fa229be2f62d49e89032e
salahelfatimi <salahfatimi16@gmail.com>
2024-11-27 09:45:10 +0100

    elete text

--- src/components/pages/header/page.jsx
+++ src/components/pages/header/page.jsx
@@ -83,7 +83,6 @@ import Testimonial from "../testimonial/page";
                     <p className=" text-white text-center  lg:text-xl">Vos résultats ne reflètent pas vos ambitions.</p>
                     <Image src={'/appel/logoyellow.png'} width={100} height={100} alt="trafic genius" className=" animate-bounce"    title="trafic genius" />
                     <h3 className=" text-white  lg:text-xl text-center">Nous souhaitons connaître qui vous êtes, les obstacles qui s&apos;abattent les résultats que vous souhaitez obtenir.</h3>
-                    <p className=" text-white  lg:text-xl text-center  font-medium">Maintenant vous pouvez enfin obtenir plus de prospects, dans un seul objectif celui de réaliser plus de vente grâce un vrai marketing rapide, puissant et scalable</p>
                 </div>    
             </div>
 

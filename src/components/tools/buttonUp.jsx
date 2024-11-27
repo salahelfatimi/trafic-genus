8475ff87a5380abec49fb7df1683af449a5f334c
salahelfatimi <salahfatimi16@gmail.com>
2024-11-27 10:54:21 +0100

    add

--- src/components/tools/buttonUp.jsx
+++ src/components/tools/buttonUp.jsx
@@ -1,46 +1,17 @@
-"use client"
-import { ArrowUp } from "lucide-react";
-import { useEffect, useState } from "react";
+import { MessageSquareText, Phone } from "lucide-react";
+import Link from "next/link";
 
-export default function ButtonUp() {
-  const [isVisible, setIsVisible] = useState(false);
-
-  useEffect(() => {
-    function handleScroll() {
-      const scrollPosition = window.scrollY;
-      const section = document.getElementById("top");
-      const bottomThreshold = 25;
-
-      if (section) {
-        setIsVisible(scrollPosition > section.offsetTop + bottomThreshold);
-      }
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []); 
-
-  function scrollToSection() {
-    const section = document.getElementById("top");
-    if (section) {
-      window.scrollTo({
-        top: section.offsetTop,
-        behavior: "smooth",
-      });
-    }
-  }
-
-  return (
-    <>
-      
-        <button
-          onClick={scrollToSection}
-          className={`${isVisible?" border-4 border-[#F2FD01] shadow-2xl":"text-[#ff9825]/0"}  fixed z-40  bg-[#000]  p-2 rounded-full  animate-bounce duration-1000 transition ${isVisible?"bottom-8 right-12" : "bottom-8 right-12 bg-[#FFECCC]/0"}`}
-        >
-          <ArrowUp size={30} className={` duration-1000 transition ${isVisible?"text-[#F2FD01]":"text-[#ff9825]/0"}`} />
-        </button>
-     
-    </>
-  );
+export default function PhoneIcon(){
+    return(
+        <div className=" z-50 flex gap-2 items-center    fixed bottom-4 right-4 animate-bounce">
+        <span className="   text-xs  text-center text-black select-none  border-2 border-black bg-[#EFFF0A] rounded-full items-center justify-center py-2 px-4">
+            <div>Avez vous besoin d&apos;aide? </div>
+            <strong>Appelez-nous</strong>
+        </span>
+        <Link href={'https://wa.me/0762911734'} target="_blank" className="bg-black border-2 border-[#EFFF0A] w-fit p-2 rounded-full ">
+          <MessageSquareText  size={35} className={` duration-1000 transition text-[#F2FD01]`} />
+        </Link>
+        
+      </div>
+    )
 }

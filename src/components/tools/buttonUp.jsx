f49ddccd5c1f60cefea7acf5791494c8f72c7a4a
salahelfatimi <salahfatimi16@gmail.com>
2024-11-27 10:57:14 +0100

    add wattsa

--- src/components/tools/buttonUp.jsx
+++ src/components/tools/buttonUp.jsx
@@ -8,8 +8,8 @@ export default function PhoneIcon(){
             <div>Avez vous besoin d&apos;aide? </div>
             <strong>Appelez-nous</strong>
         </span>
-        <Link href={'https://wa.me/0762911734'} target="_blank" className="bg-black border-2 border-[#EFFF0A] w-fit p-2 rounded-full ">
-          <MessageSquareText  size={35} className={` duration-1000 transition text-[#F2FD01]`} />
+        <Link href={'https://wa.me/0762911734'} target="_blank" className="bg-black border-2 border-[#EFFF0A] hover:bg-[#F2FD01] hover:border-black duration-500 w-fit p-2 rounded-full ">
+          <MessageSquareText  size={35} className={` duration-1000 transition text-[#F2FD01] hover:text-black stroke-[#F2FD01] hover:stroke-black`} />
         </Link>
         
       </div>

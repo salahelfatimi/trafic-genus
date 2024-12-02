5f6a74c1c70b937292fd13030b2d62e4540f1b89
salahelfatimi <salahfatimi16@gmail.com>
2024-12-02 17:56:51 +0100

    add site map

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,37 +1,35 @@
-
-
 export default async function sitemap() {
- 
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL; // Define base URL
+  let dynamicPaths = [];
 
-    return [
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
-        lastModified: new Date(),
-      },
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}appel`,
-        lastModified: new Date(),
-      }
-      ,
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}boutique`,
-        lastModified: new Date(),
-      }
-      ,
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}mentions-legales`,
-        lastModified: new Date(),
-      }
-      ,
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}conditions-generales-de-vente`,
-        lastModified: new Date(),
-      }
-      ,
-      {
-        url: `${process.env.NEXT_PUBLIC_BASE_URL}clause-de-non-responsabilite`,
-        lastModified: new Date(),
-      }
-      
-    ];
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_WORDPRESS_API_URL}/wp-json/wp/v2/posts?_embed&per_page=100`
+    );
+
+    if (response.ok) {
+      const posts = await response.json();
+      dynamicPaths = posts.map((post) => ({
+        url: `${baseUrl}/blogs/${encodeURIComponent(
+          post.title.rendered.trim().replace(/[/%\s]+/g, "-")
+        )}/${post.id}`,
+        lastModified: new Date(post.modified),
+      }));
+    } else {
+      console.error("Failed to fetch posts. Response not OK.");
+    }
+  } catch (error) {
+    console.error("Failed to fetch posts for the sitemap:", error);
   }
+
+  return [
+    { url: `${baseUrl}`, lastModified: new Date() },
+    { url: `${baseUrl}/appel`, lastModified: new Date() },
+    { url: `${baseUrl}/boutique`, lastModified: new Date() },
+    { url: `${baseUrl}/mentions-legales`, lastModified: new Date() },
+    { url: `${baseUrl}/conditions-generales-de-vente`, lastModified: new Date() },
+    { url: `${baseUrl}/clause-de-non-responsabilite`, lastModified: new Date() },
+    { url: `${baseUrl}/blogs`, lastModified: new Date() },
+    ...dynamicPaths,
+  ];
+}

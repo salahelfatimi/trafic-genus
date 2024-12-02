ecb0fb900dc4ffa7913d8d450b1ed15061defce5
salahelfatimi <salahfatimi16@gmail.com>
2024-12-02 18:58:00 +0100

    add

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -9,7 +9,7 @@ export default function Loading() {
           <span className={`font-extrabold text-center items-center gap-3 md:text-5xl text-4xl  flex flex-col lg:flex-row justify-center text-white  `}>
            
             <span className=" text-[#fff] flex  items-center justify-center   ">
-                TRAFIC GEN <Image src={'/logo.png'} className="animate-bounce w-24" width={500} height={500}/> US
+                TRAFIC GEN <Image src={'/logo.png'} className="animate-bounce w-16" width={500} height={500}/> US
             </span>
           </span>
         </div>

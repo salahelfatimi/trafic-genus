3e2fe6b148edc0dbd574b110c17db9b49250e4f5
salahelfatimi <salahfatimi16@gmail.com>
2025-01-17 18:02:49 +0100

    feat: enhance cookie consent message with new design and add cookie icon

--- src/components/tools/cookies/message.jsx
+++ src/components/tools/cookies/message.jsx
@@ -32,14 +32,16 @@ export default function MessageCookies() {
       <>
         {isVisible && (
             <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 text-white p-4 z-50">
-            <div className="container mx-auto max-w-md bg-gray-800 rounded-lg shadow-lg p-6">
-            <p className="text-sm text-center mb-4"> Ce site utilise des cookies pour améliorer votre expérience. Consultez notre{" "} <Link href="/politique-de-confidentialite" className="text-primary underline hover:text-primary" > politique de confidentialité </Link> . </p>
-              <div className="flex gap-4 justify-center">
+            <div className="container flex flex-col items-center mx-auto max-w-md bg-gray-800 rounded-lg shadow-lg p-6">
+              <img src="/cookie.svg" className=" w-16" alt="" />
+              <p className=" font-bold text-2xl ">cookies consentement</p>
+              <p className="text-sm text-center mb-4"> Ce site utilise des cookies pour améliorer votre expérience. Consultez notre{" "} <Link href="/politique-de-confidentialite" className="text-primary underline hover:text-primary" > politique de confidentialité </Link> . </p>
+                <div className="flex gap-4 justify-center">
                     <button className="bg-primary text-gray-900 font-semibold py-2 px-4 rounded hover:bg-transparent border-2 border-primary hover:text-primary transition" onClick={() => handleCookiesDecision(true)} > Accepter </button>
                     <button className="bg-gray-600 text-white font-semibold py-2 px-4 rounded hover:bg-gray-700 transition" onClick={() => handleCookiesDecision(false)} > Refuser </button>
                 </div>
+              </div>
             </div>
-          </div>
         )}
         {cookiesAccepted && (
           <>
